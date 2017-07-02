58c9331f8204fd1b0d5a4f51cb8833465e8db7e0
scripore <prongzhang@gmail.com>
2017-07-02 16:22:03 -0400

    create views for activity and fragment

---
+++ CriminalIntent/app/src/main/java/com/example/mac/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package com.example.mac.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by mac on 7/2/17.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
