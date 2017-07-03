1caea036f07e911335e56d4e703edd86d58b3b9f
scripore <prongzhang@gmail.com>
2017-07-03 01:29:07 -0400

    add constraints for ImageView

--- CriminalIntent/app/src/main/java/com/example/mac/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/mac/criminalintent/CrimeListFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -38,7 +39,7 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecycleView.setAdapter(mAdapter);
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private TextView mTitleTextView;
         private TextView mDateTextView;
@@ -46,8 +47,22 @@ public class CrimeListFragment extends Fragment {
 
         public CrimeHolder (LayoutInflater inflater, ViewGroup parent) {
             super (inflater.inflate(R.layout.list_item_crime, parent, false));
+            itemView.setOnClickListener(this);
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+        }
+
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(),
+                    mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+
         }
 
     }
@@ -67,7 +82,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(CrimeHolder holder, int position) {
-
+            Crime crime = mCrimes.get(position);
+            holder.bind(crime);
         }
 
         @Override
