3a34e7feba2191d8ee9eaa156366cfc1d83d14b9
scripore <prongzhang@gmail.com>
2017-07-02 22:23:38 -0400

    utilize RecyclerView

--- CriminalIntent/app/src/main/java/com/example/mac/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/mac/criminalintent/CrimeListFragment.java
@@ -1,12 +1,29 @@
 package com.example.mac.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by mac on 7/2/17.
  */
 
 public class CrimeListFragment extends Fragment {
-    // nothing yet
+    private RecyclerView mCrimeRecycleView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecycleView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+
+        mCrimeRecycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 
 }
